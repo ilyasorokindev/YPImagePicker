54abdd0eb6bc85a5d59e41f5018c0d6181aaa927
Ilya Sorokin <ilya.sorokin@sigma.software>
2021-07-12 16:54:49 +0300

    spm > Update

--- Sources/YPImagePicker/Bundle+Extension.swift
+++ Sources/YPImagePicker/Bundle+Extension.swift
@@ -13,21 +13,22 @@ private class BundleFinder {}
 public extension Bundle {
     
     static var resourceBundle: Bundle = {
- 
-        let bundleName = "Resources_Resources"
-        let candidates = [
-            Bundle.main.resourceURL,
-            Bundle(for: BundleFinder.self).resourceURL,
-            Bundle.main.bundleURL,
-            Bundle(for: BundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
-        ]
-        for candidate in candidates {
-            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
-            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
-                return bundle
-            }
-        }
-        fatalError("unable to find bundle named \(bundleName)")
+  
+        return Bundle(for: BundleFinder.self)
+//        let bundleName = "Resources_Resources"
+//        let candidates = [
+//            Bundle.main.resourceURL,
+//            Bundle(for: BundleFinder.self).resourceURL,
+//            Bundle.main.bundleURL,
+//            Bundle(for: BundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
+//        ]
+//        for candidate in candidates {
+//            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+//            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+//                return bundle
+//            }
+//        }
+//        fatalError("unable to find bundle named \(bundleName)")
     }()
     
 }
